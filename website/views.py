91cf844945885632ec8eadd65ba638f4bb518109
Amandasdj <amanda_sj12@hotmail.com>
2019-09-25 22:06:11 -0300

    Arrumar html e models

--- website/views.py
+++ website/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from website.models import *
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+       data = Pessoas()
+       data.nome = request.POST['nome']
+       data.sobrenome =request.POST['sobrenome']
+       data.data_de_nascimento = request.POST['data_de_nascimento']
+       data.cpf = request.POST['cpf']
+       data.cep = request.POST['cep']
+       data.item_de_doacao = request.POST['item_de_doacao']
+       data.save()
+
+
+   
+    return render(request, 'index.html') 
 def sobre(request):
-    return render(request, 'Sobre.html') 
+  return render(request, 'Sobre.html') 
