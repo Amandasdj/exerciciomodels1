91ab5fb373ceb343c764692504ac2e414742579d
Amandasdj <amanda_sj12@hotmail.com>
2019-09-25 22:04:11 -0300

    produção das models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoas(models.Model):
+   nome = models.CharField(max_length=255,  verbose_name='Nome')
+   sobrenome = models.TextField()
+   data_de_nascimento = models.DateField()
+   cpf = models.CharField(max_length=14)
+   cep = models.CharField(max_length=9) 
+   item_de_doacao = models.CharField(max_length=255) 
+
+   data_de_criacao =  models.DateTimeField()
+   ativo = models.BooleanField(default=True)
